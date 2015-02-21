b741a0a8e32e21596241105a151b92b46e4b965b
Jim Holmström <jim.holmstroem@gmail.com>
2015-02-21 13:01:57 +0100

    Update and rename jim to jim/memoize.py

---
+++ jim/memoize.py
@@ -0,0 +1,24 @@
+from __future__ import division, print_function
+
+from functools import (
+    wraps,
+)
+
+def memoized(directory='data'):
+    def _memoized(f):
+        import os
+        directory = 'data'
+    
+        @wraps(f)
+        def memoized_f(arg):
+            data_path = os.path.join(directory, arg)
+            if not(os.path.exists(data_path)):
+                with open(data_path, 'w') as f:
+                    f.write(f(arg))
+    
+            with open(data_path, 'r') as f:
+                return f.read()
+  
+        return memoized_f
+
+    return _memoized
