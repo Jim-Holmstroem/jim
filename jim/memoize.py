ed4145bf2680e40b5cd3f6c50d70b6a800aaca9b
Jim <jim.holmstroem@gmail.com>
2015-02-21 18:38:17 +0100

    makes the directories where the memoized data is located

--- jim/memoize.py
+++ jim/memoize.py
@@ -5,23 +5,28 @@ from functools import (
 )
 
 from jim import function
+from jim.os import mkdirp
 
 
 def memoized(directory='data'):
+    mkdirp(directory)
+
     def _memoized(f):
         import os
-        directory = 'data'
-    
+
+        function_directory = os.path.join(directory, function.fullname(f))
+        mkdirp(function_directory)
+
         @wraps(f)
         def memoized_f(arg):
-            data_path = os.path.join(directory, function.fullname(f), arg)
+            data_path = os.path.join(function_directory, arg)
             if not(os.path.exists(data_path)):
                 with open(data_path, 'w') as f:
                     f.write(f(arg))
-    
+
             with open(data_path, 'r') as f:
                 return f.read()
-  
+
         return memoized_f
 
     return _memoized
